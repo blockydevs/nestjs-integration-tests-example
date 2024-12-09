4a8f7846a7149efd4a3b9c687f8219cb88ca8504
stanislawkurzypBD <stanislaw.kurzyp@blockydevs.com>
2024-12-09 10:28:58 +0100

    removed Date of completion from OrderEntity, switched to storing order value as bigint, switched to logger

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -16,18 +16,10 @@ export class OrderEntity {
   @Column({ name: 'cart_id' })
   cartId: string;
 
-  @Column({
-    type: 'timestamp',
-    default: () => 'CURRENT_TIMESTAMP',
-    name: 'date_completed',
-    nullable: false,
-  })
-  dateCompleted: Date;
-
   @JoinColumn({ name: 'customer_id' })
   @ManyToOne(() => CustomerEntity)
   customer: CustomerEntity;
 
-  @Column({ type: 'float', name: 'total_value' })
-  totalValue: number;
+  @Column({ type: 'bigint', name: 'total_value' })
+  totalValue: bigint;
 }
--- src/order/order.interface.ts
+++ src/order/order.interface.ts
@@ -2,7 +2,6 @@ import { Customer } from '../customer/customer.interface';
 
 export interface OrderCreateCommand {
   cartId: string;
-  dateCompleted: Date;
   customer: Customer;
-  totalValue: number;
+  totalValue: bigint;
 }
